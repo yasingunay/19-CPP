46570c46cedb0bb701cc23fd46fef709f64de474
yasingunay <chalouper@icloud.com>
2023-06-15 17:05:50 +0200

    visual improvements

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,34 +6,27 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 14:03:34 by ygunay            #+#    #+#             */
-/*   Updated: 2023/06/15 16:57:40 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/06/15 17:04:30 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <iostream>
 #include <string>
 #include <cctype>
 
-
 int main(int ac, char **av) 
 {
-
 	if(ac > 1)
 	{
 		for(int i =1; i < ac; i++)
 		{
 			std::string 	str = av[i];
-
-				int length = str.length();
-
-				for(int j =0; j < length; j++)
-				{
-					char c = toupper(str[j]);
-					std:: cout << c;
-				}
-					
-					
+			int length = str.length();
+			for(int j =0; j < length; j++)
+			{
+				char c = toupper(str[j]);
+				std:: cout << c;
+			}			
 		}
 		std::cout << std:: endl;
 	}
