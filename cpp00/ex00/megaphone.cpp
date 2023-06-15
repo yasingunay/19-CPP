a08a3582a22025e85105f0682f177318a309f847
yasingunay <chalouper@icloud.com>
2023-06-15 17:02:58 +0200

    last

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,16 +6,39 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 14:03:34 by ygunay            #+#    #+#             */
-/*   Updated: 2023/06/15 14:03:37 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/06/15 16:57:40 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
 
 int main(int ac, char **av) 
 {
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std:: endl ;
+
+	if(ac > 1)
+	{
+		for(int i =1; i < ac; i++)
+		{
+			std::string 	str = av[i];
+
+				int length = str.length();
+
+				for(int j =0; j < length; j++)
+				{
+					char c = toupper(str[j]);
+					std:: cout << c;
+				}
+					
+					
+		}
+		std::cout << std:: endl;
+	}
+	if(ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std:: endl ;
 	return 0;
 }
+
