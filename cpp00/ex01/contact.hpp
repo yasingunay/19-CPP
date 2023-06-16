b364e3d8cea69c186ee28e7c12dec5a04b8f07a8
yasingunay <chalouper@icloud.com>
2023-06-16 13:38:05 +0200

    up

--- cpp00/ex01/contact.hpp
+++ cpp00/ex01/contact.hpp
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 10:57:00 by ygunay            #+#    #+#             */
-/*   Updated: 2023/06/16 11:51:34 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/06/16 13:37:19 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ class Contact {
 	private:
 		std::string first_name;
 		std::string last_name;
+		std::string nickname;
 		std::string number;
+		std::string secret;
 
 	public:
 		void set_first_name (const std::string& new_first_name){
@@ -31,10 +33,18 @@ class Contact {
 			last_name = new_last_name;
 		}
 
+		void set_nickname (const std::string& new_nickname){
+			nickname = new_nickname;
+		}
+
 		void set_number (const std::string& new_number){
 			number = new_number;
 		}
-				
+
+		void set_secret (const std::string& new_secret){
+			secret = new_secret;
+		}
+		
 		void introduce(){
 			std::cout << "Hello my name is " << first_name << " My surname is " << last_name << " My phone number is " << number << "." << std::endl;
 		}
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,20 +6,35 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 11:43:30 by ygunay            #+#    #+#             */
-/*   Updated: 2023/06/16 11:46:27 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/06/16 13:33:25 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "contact.hpp"
 
 int main(){
-	Contact person1;
-	person1.set_first_name("Yasin");
-	person1.set_last_name("Gunay");
-	person1.set_number("0491192425");
+
+	std:: string command;
+
+	std::cout << "Enter a command(ADD, SEARCH or EXIT)" << std::endl;
+	std:: getline(std::cin, command);
+	
+	if(command == "ADD")
+	{
+		Contact person1;
+		std::cout << "Enter a first name: ";
+		std:: string first_name;
+		std:: getline(std::cin, first_name);
+		person1.set_first_name(first_name);
+		person1.set_last_name("Gunay");
+		person1.set_number("0491192425");
+
+		person1.introduce();
+	
+	}
 	
 	
-	person1.introduce();
+	
 
 	return 0;
 	
